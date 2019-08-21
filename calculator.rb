ce8856f9b386a055c004ec6560ae9fee4bd244b4
Ted Williams <tboxyz@gmail.com>
2019-08-21 02:16:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,6 @@ second_number = 6
 
 sum = first_number + second_number
 
-difference = second_number - first_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = second_number / first_number
